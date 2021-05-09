3372b2f20ded65fe4ba75cdee7bbf7376578d06b
Charmaine Tabilas <charmaine.tabilas@devin-the-dude.myfiosgateway.com>
2021-05-09 11:57:38 -0400

    created basic config and simple examples

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+  1.
+*/
+console.log("1. ");
+var example = "string";
+console.log({ example: example });
+console.log("\n\n -------------------------------- \n\n");
+/*
+  2.
+*/
+console.log("2. ");
---
+++ index.ts
@@ -0,0 +1,78 @@
+/**
+ * Basic JS Types
+ * - string
+ * - number
+ * - boolean
+ * - object
+ * - undefined
+ * - null
+ * - symbol
+ * 
+ * Technically can include:
+ * - BigInt
+ * - function
+ * 
+ * 
+ * JS is loosely typed and you can get away with anything
+ * JS not v close to memory so types aren't very specific
+ * 
+ * Write ts same way we write js
+ * ts is js with extra features
+ */
+
+/* 
+  1. Implicit Declaration
+*/ 
+console.log(`1. Implicit Declaration `)
+let example = "string"
+// example = 1
+// example = 'typescript'
+
+console.log(`\n\n -------------------------------- \n\n`)
+
+
+/* 
+  2. Explicit Declaration
+*/ 
+console.log(`2. Explicit Declaration`)
+let greeting: string;
+
+greeting = "Hello, HRC"
+
+console.log(`\n\n -------------------------------- \n\n`)
+
+
+
+/* 
+  3. Running TSC
+*/ 
+console.log(`3. Running TSC`)
+// 1. compile
+// 2. run js
+// viola!
+
+console.log({ example, greeting })
+
+console.log(`\n\n -------------------------------- \n\n`)
+
+
+
+
+/* 
+  2. Explicit Declaration
+*/ 
+console.log(`2. Explicit Declaration`)
+
+
+console.log(`\n\n -------------------------------- \n\n`)
+
+
+
+
+/* 
+  2. Explicit Declaration
+*/ 
+console.log(`2. Explicit Declaration`)
+
+
+console.log(`\n\n -------------------------------- \n\n`)
