7eec40cc148c8a6518d3daebef7bcae45f98efec
Charmaine Tabilas <charmaine.tabilas@devin-the-dude.myfiosgateway.com>
2021-05-09 12:59:49 -0400

    completed vanilla exmaple

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # vanilla typescript demo
+
+### Slides
+[TypeScript 2021 Slides](https://docs.google.com/presentation/d/18Pdee2LDhGUxCWnw0C_7IQVOgrAuGVHsziKI0C9eczo/edit#slide=id.gd86c7a983c_0_507)
+
+### Helpful Documentation
+[Oficial TS Docs](https://www.typescriptlang.org/index.html)
+[TS Dos and Don'ts](https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html)
+[Intro to TS Article](https://dev.to/racheladaw/intro-to-typescript-4d8k)
+[Types vs Interface Deep Dive](https://dev.to/stereobooster/typescript-type-vs-interface-2n0c)
+
--- index.ts
+++ index.ts
@@ -25,18 +25,19 @@
   1. TS Types
   https://www.typescriptlang.org/docs/handbook/basic-types.html
 
-  Boolean
-Number
-String
-Array
-Tuple
-Enum
-Unknown
-Any
-Void
-Null and Undefined
-Never
-Object
+  - Boolean
+  - Number
+  - String
+  - Array
+  - Tuple
+  - Enum
+  - Unknown
+  - Any
+  - Void
+  - Null 
+  - Undefined
+  - Never
+  - Object
 */ 
 console.log(`1. TS Types`)
 
@@ -62,6 +63,12 @@ let greeting: string;
 
 greeting = "Hello, HRC"
 
+// let numbers: number[] 
+let numbers: Array<number>
+numbers = [1, 2, 3]
+numbers = ['1', 2]
+
+
 console.log(`\n\n -------------------------------- \n\n`)
 
 
@@ -77,8 +84,6 @@ console.log(`3. Running TSC`)
 console.log({ example, greeting })
 
 console.log(`\n\n -------------------------------- \n\n`)
-
-
 
 
 
@@ -116,9 +121,142 @@ console.log(`\n\n -------------------------------- \n\n`)
 
 
 /* 
-  2. Explicit Declaration
+  3. Unions
 */ 
-console.log(`2. Explicit Declaration`)
+console.log(`3. Unions`)
+
+let unknownDataType: string | undefined
+type CaliforniaTeams = 'Sacremento Kings' | 'Golden State Warriors' | 'LA Lakers' | 'San Francisco 49ers' | 'San Francisco Giants'
+// const favoriteTeam: CaliforniaTeams 
+
+
+
+console.log(`\n\n -------------------------------- \n\n`)
+
+
+
+/* 
+  3. Intersections
+*/ 
+console.log(`3. Intersections`)
+type UserResponse = {
+  uuid: number,
+  email: string
+}
+
+type ErrorResponse = {
+  status: 200 | 400 | 500
+  errorLog: string | undefined
+}
+
+type APIResponse = UserResponse & ErrorResponse
+
+
+console.log(`\n\n -------------------------------- \n\n`)
+
+/* 
+  3. Optional Properties
+*/ 
+console.log(`3. Optional Properties`)
+
+// {
+//   payload?: string
+// }
+
+// vs
+
+// {
+//   payload: string | undefined
+// }
+
+type PostPayload = {
+  route: string
+  message?: string
+  // message: string | undefined
+}
+
+const formPayload: PostPayload = {
+  route: '/api/v1/invite'
+}
+
+
+
+
+
+console.log(`\n\n -------------------------------- \n\n`)
+
+
+/* 
+  3. Handling Unknown Data
+*/ 
+console.log(`3. Handling Unknown Data`)
+
+type MiscObj = {
+  [key: string]: number
+  // [key: number]: string
+}
+
+const unknownObj: MiscObj = {
+  // someKey: 3,
+  // 3: 3
+  // 3: '3'
+}
+
+console.log(`\n\n -------------------------------- \n\n`)
+
+/* 
+  Functions
+*/ 
+console.log(`Functions`)
+
+// let numberFactory = ({ valOne, valTwo }: { valOne: number, valTwo: number }): number => {
+//   const result = valOne + valTwo
+
+//   return result
+// }
+
+
+let numberFactory = ({ valOne, valTwo }: { valOne: number[], valTwo: number[] }): number => {
+  // const result = valOne + valTwo
+
+  // // my fav thing about TS 
+  // const result 
+  // console.log({ result })
+  // return result
+}
+
+// const numberFactoryResult = numberFactory({ valOne: [1, 2], valTwo: [3, 4] })
+
+/* 
+  Void
+*/ 
+console.log(`Void`)
+
+// let numberFactory = ({ valOne, valTwo }: { valOne: number[], valTwo: number[] }): void => {
+//   // const result = valOne + valTwo
+
+//   // my fav thing about TS 
+//   const result = [...valOne, ...valTwo]
+
+//   console.log({ result })
+// }
+// const numberFactoryResult = numberFactory({ valOne: [1, 2], valTwo: [3, 4] })
+
+console.log(`\n\n -------------------------------- \n\n`)
+
+
+/* 
+  Void 
+*/ 
+console.log(`Any 🙄`)
+// NOTE: good use case is when you bring in a 3rd party library that doesn't have its own TS library
+//  essentially says to ignore TS
+let tree: any = '1'
+tree = 1
+tree = {
+  type: 'birch'
+}
+tree = ['spring']
 
 
 console.log(`\n\n -------------------------------- \n\n`)
